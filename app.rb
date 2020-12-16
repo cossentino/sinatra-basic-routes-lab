dbe138c74182ac5d607268b14618e00eb92a03a9
Ian Cossentino <icossentino@gmail.com>
2020-12-16 14:25:40 -0500

    commit tree 97d9da8c2f1af1df5240c5968f37123c3e463cd1

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Ian"
+    end
+
+    get '/hometown' do
+        "My hometown is Roswell, Georgia"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is bluebird"
+    end
+
+
 end
